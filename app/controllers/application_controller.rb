092bd288e841a3ef05037b7ee4f38025b35e9f31
mametaro99 <bearsmichimato99@gmail.com>
2024-09-09 22:53:52 +0900

    身分証明書のカラムをユーザモデルに登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # /users/sign_up
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:card_front,:cart_back])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :test_answers, dependent: :destroy
+  has_one_attached :card_front # 証明書表面
+  has_one_attached :card_back # 証明書裏面
 end
